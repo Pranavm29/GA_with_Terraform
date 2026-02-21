d9cd3bd3ba9956d0cfd406ef5a698e9bc6ee4bc2
Pranav Mali <pranavmali32@gmail.com>
2026-02-21 15:07:38 +0530

    unit test for flask app

---
+++ tests/test_app.py
@@ -0,0 +1,20 @@
+import unittest
+from app.app import app
+
+
+class FlaskAppTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_home(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_health(self):
+        response = self.app.get("/health")
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
